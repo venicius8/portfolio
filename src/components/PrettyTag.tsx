f8bdd7b8f5f2290050180b0eca137110b7f3b1d7
venicius8 <veniciusmiguel@protonmail.com>
2026-08-27 10:49:58 -0300

    fix: add margin to PrettyTag

--- src/components/PrettyTag.tsx
+++ src/components/PrettyTag.tsx
@@ -1,10 +1,10 @@
 function PrettyTag({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="my-10">
       <span className="text-cyan-300">&lt;</span>
       {children}
       <span className="text-cyan-500"> /&gt;</span>
-    </>
+    </div>
   );
 }
 
